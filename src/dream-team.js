23de320c90342753bf2d5d43e5f923e49a15ec85
Tanya Khreshchykava <tanya.khreshchykava@gmail.com>
2020-03-22 15:53:14 -0400

    feat: add dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+
+	if (Array.isArray(members)) {
+		var firstLetters = [];
+		members.forEach((el) => {
+			if (typeof el === "string" && el !== null) {
+				firstLetters.push(el.trim()[0].toUpperCase());
+			}
+		});
+		return firstLetters.sort().join("");
+	} else {
+		return false;
+	}
+}
+
